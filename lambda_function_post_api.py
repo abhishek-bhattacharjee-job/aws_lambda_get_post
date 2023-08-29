7f89dda20b1717d8fe19a01daf8090055e1e52d9
Abhishek Bhattacharjee <abhishek.bhattacharjee.job@gmail.com>
2023-08-29 08:44:42 +0530

    post api change

--- lambda_function_post_api.py
+++ lambda_function_post_api.py
@@ -4,7 +4,11 @@ def lambda_handler(event, context):
     req_data = json.loads(event["body"])
     var1 = req_data["var1"]
     var2 = req_data["var2"]
+    response_headers = {
+        'Content-Type': 'application/json'
+    }
     return {
         'statusCode': 200,
+        'headers': response_headers,
         'body': json.dumps({ "var1" : var1, "var2" : var2 })
     }
